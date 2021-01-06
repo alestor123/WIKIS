7d6b76adbe3e01eb8d0ee6c9b3dccc4c8c49c99e
alestor123 <alestoraldous@gmail.com>
2021-01-06 15:44:07 +0530

    to lower case

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 module.exports = async topic => {
-var inp = topic.replace(' ','_'),
+var inp = topic.replace(' ','_').toLowerCase(),
 data = await axios.get('https://en.wikipedia.org/api/rest_v1/page/summary/'+inp);
 return { 
     title:data.data.title,
