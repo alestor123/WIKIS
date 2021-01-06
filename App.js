ca87cb42fddacf836a101329b26756f6f5f1b33b
alestor123 <alestoraldous@gmail.com>
2021-01-06 15:00:10 +0530

    added api

---
+++ App.js
@@ -0,0 +1,11 @@
+var axios = require('axios');
+module.exports = async topic => {
+var inp = topic.replace(' ','_')
+var data = await axios.get('https://en.wikipedia.org/api/rest_v1/page/summary/'+inp)
+return { 
+    title:data.data.title,
+    desc:data.data.extract,
+    lang:data.data.lang,
+    timestamp:data.data.timestamp
+}
+}
