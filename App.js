d3c1b13a6ade7446832298fd491899ab2a8f07aa
alestor123 <alestoraldous@gmail.com>
2021-01-06 15:38:48 +0530

    added main code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 module.exports = async topic => {
-var inp = topic.replace(' ','_')
-var data = await axios.get('https://en.wikipedia.org/api/rest_v1/page/summary/'+inp)
+var inp = topic.replace(' ','_'),
+data = await axios.get('https://en.wikipedia.org/api/rest_v1/page/summary/'+inp);
 return { 
     title:data.data.title,
     desc:data.data.extract,
---
+++ cli.js
@@ -0,0 +1,13 @@
+var wikis = require('./App'),
+chalk = require('chalk'),
+figlet = require('figlet'),
+options =  require('minimist')(process.argv.slice(2));
+(async () => {
+var outp = await wikis(options.f || options.find || 'github')
+figlet(outp.title, (data,err) => {
+if(err) throw err
+console.log(chalk.green(data))
+console.log(outp.desc)
+console.log(outp.lang)
+})
+})();
